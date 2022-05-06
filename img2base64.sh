d47a7898c1e9cfe9066b2a3f13c399a627f02dea
Vincent Schwarz <github@gibtsfuerkleingeld.de>
2022-05-06 23:37:27 +0200

    updated ref tag for markdown

--- img2base64.sh
+++ img2base64.sh
@@ -4,5 +4,11 @@ for f in "$@"
 do
 	TYPE=$(file --mime-type -b $1)
 	ENC=$(base64 $1)
-	echo "data:$TYPE;base64,$ENC" | pbcopy
+	REF=""
+	FILENAME=$(basename $1)
+	if [ true ]
+	then
+		REF="![$FILENAME][img_$FILENAME]\n[img_$FILENAME]"
+	fi
+	printf "$REF: data:$TYPE;base64,$ENC" | pbcopy
 done
